51e1bcf232ce8df745d71008ee7b5252b3b13189
Joshua Goon <j.j.jappiecraft@gmail.com>
2023-03-03 02:05:30 -0600

    load font

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+
+import type { AppProps } from "next/app";
+
+import { DM_Sans } from "next/font/google";
+
+const dm_sans = DM_Sans({
+	weight: ["400", "500", "700"],
+	subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<main className={dm_sans.className}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
