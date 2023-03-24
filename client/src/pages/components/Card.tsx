fd0101e6156624c3cb9e0ea0359bd8bd3af59bd9
Joshua Goon <j.j.jappiecraft@gmail.com>
2023-03-23 20:52:12 -0500

    changes

---
+++ client/src/pages/components/Card.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import Image from "next/image";
+
+const Card: FC = () => {
+	return (
+		<>
+			<div className="w-[260px] h-[200px] bg-slate-400">
+				<div>
+					{/* <Image
+						src="https://cultivatedculture.com/wp-content/themes/x5-child/assets/images/templates/template6.jpg"
+						alt="Resume"
+						fill
+					/> */}
+				</div>
+				<div>Bottom</div>
+			</div>
+		</>
+	);
+};
+
+export default Card;
