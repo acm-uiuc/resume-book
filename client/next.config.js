f64f160cc13ac19f5fbe446fa046bddd485679d1
Joshua Goon <j.j.jappiecraft@gmail.com>
2023-03-23 21:30:17 -0500

    temp for image

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	images: {
+		domains: ["cultivatedculture.com"],
+	},
 };
 
 module.exports = nextConfig;
-
-module.exports = {
-	images: {
-		domains: ["tmdb.org", "themoviedb.org"],
-	},
-};
