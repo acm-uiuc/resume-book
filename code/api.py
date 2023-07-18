3f4a41abad83114a9ba0c246f031abdcc53def70
Dev Singh <dsingh14@illinois.edu>
2023-07-18 00:08:38 +0000

    loggin

--- code/api.py
+++ code/api.py
@@ -1,4 +1,5 @@
 def lambda_handler(event, context):
+    print(event['httpMethod'], event['pathParameters'], event['queryStringParameters'])
     return {
         "statusCode": 200,
         "body": "UP"
