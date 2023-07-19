f2bcf4604a7d82db9ccd418e12b9ce00eeaceea2
Dev Singh <dsingh14@illinois.edu>
2023-07-19 22:42:12 +0000

    remove direct s3 copy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-aws s3 cp docs/swagger-resume-book.yml s3://acm-uiuc-code/
 sam build --template-file cloudformation/lambda.yml
 sam deploy --no-confirm-changeset --no-fail-on-empty-changeset
