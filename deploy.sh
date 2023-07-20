547593f9851f1d8de2ac144e596b824ba57d4cfc
Dev Singh <dsingh14@illinois.edu>
2023-07-20 04:12:06 +0000

    add

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sam build --template-file cloudformation/lambda.yml
-sam deploy --no-confirm-changeset --no-fail-on-empty-changeset
+sam build --template-file cloudformation/lambda.yml && sam deploy --no-confirm-changeset --no-fail-on-empty-changeset
