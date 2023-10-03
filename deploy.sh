5e00a33367948da75285ca56b1aa5d9c88807b79
Dev Singh <dsingh14@illinois.edu>
2023-10-03 13:45:20 -0500

    force setting runenv

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sam build --template-file cloudformation/lambda.yml && sam deploy --no-confirm-changeset --no-fail-on-empty-changeset
+sam build --template-file cloudformation/lambda.yml --parameter-overrides ParameterKey=RunEnvironment,ParameterValue=prod
+sam deploy --no-confirm-changeset --no-fail-on-empty-changeset
