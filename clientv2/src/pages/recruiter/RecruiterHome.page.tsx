358451f645395d78e24da6d16381266aeac2927a
Dev Singh <dsingh14@illinois.edu>
2024-07-15 14:46:29 -0400

    begin building the recruiter flow

--- clientv2/src/pages/recruiter/RecruiterHome.page.tsx
+++ clientv2/src/pages/recruiter/RecruiterHome.page.tsx
@@ -1,25 +1,61 @@
-import { Button, Title } from '@mantine/core';
+import { useState } from 'react';
+import { Title, Container } from '@mantine/core';
 import { useAuth } from '@/components/AuthContext';
 import { HeaderNavbar } from '@/components/Navbar';
+import DegreeFilter, { Filters } from '@/components/SearchProfiles';
+import { useApi } from '@/util/api';
+import { notifications } from '@mantine/notifications';
+import FullScreenLoader from '@/components/AuthContext/LoadingScreen';
 
+
+function showErrorNotification(title?: string) {
+  notifications.show({
+    title: title || "Failed to Fetch Profiles",
+    message: "Please try again or contact the ACM@UIUC Corporate Team.",
+    color: 'red'
+  })
+}
 export function RecruiterHomePage() {
-  const { userData, getToken } = useAuth();
-  const [lastName, firstName] = userData?.name?.split(',') as string[];
-  const doAuthStuff = async () => {
-    if (!userData) {
-      return;
+  const { userData } = useAuth();
+  const [loading, setLoading] = useState<boolean>(false);
+  const api = useApi();
+
+  const handleFilter = async (filters: Filters) => {
+    let response;
+    try {
+      setLoading(true);
+      response = await api.post("/recruiter/search", filters)
+    } catch {
+      setLoading(false);
+      return showErrorNotification()
     }
-    console.log(await getToken());
+    if (response.status != 200) {
+      setLoading(false);
+      return showErrorNotification();
+    }
+    console.log(response);
+    setLoading(false);
   };
+  if (loading) {
+    return <FullScreenLoader />
+  }
   return (
     <>
       <HeaderNavbar userData={userData} />
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <Title>
-          Hello {firstName} {lastName}!
-        </Title>
-        <Button onClick={doAuthStuff}>Do Auth Stuff</Button>
-      </div>
+      <Container>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            marginBottom: '20px',
+          }}
+        >
+          <Title order={1}>Search Resume Book</Title>
+        </div>
+        <DegreeFilter onFilter={handleFilter} />
+        {/* Render filtered data here based on the filters state */}
+      </Container>
     </>
   );
 }
