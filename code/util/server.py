e467f23e887a246df5c0f1263dc8a4c54bbdb843
Dev Singh <dsingh14@illinois.edu>
2024-07-17 16:59:23 -0400

    Revert "get back to the basics"
    This reverts commit f38b2cd8bf9711871be9a5a18236c7761841b3fd.

--- code/util/server.py
+++ code/util/server.py
@@ -25,10 +25,10 @@ if RUN_ENV != "prod":
 
 cors_config = CORSConfig(allow_origin="https://resumes.acm.illinois.edu", extra_origins=extra_origins, max_age=300, allow_credentials=True, allow_headers=["authorization"])
 app = APIGatewayRestResolver(cors=cors_config)
-# session = boto3.Session(region_name=os.environ.get('AWS_REGION', 'us-east-1'))
-# secretsmanager = session.client('secretsmanager')
-# dynamodb = session.client('dynamodb')
-# db_config = get_parameter_from_sm(secretsmanager, 'infra-resume-book-db-config')
+session = boto3.Session(region_name=os.environ.get('AWS_REGION', 'us-east-1'))
+secretsmanager = session.client('secretsmanager')
+dynamodb = session.client('dynamodb')
+db_config = get_parameter_from_sm(secretsmanager, 'infra-resume-book-db-config')
 
 PROFILE_TABLE_NAME = "infra-resume-book-profile-data"
 S3_BUCKET = f"infra-resume-book-pdfs-{RUN_ENV if RUN_ENV != 'local' else 'dev'}"
@@ -37,100 +37,100 @@ S3_BUCKET = f"infra-resume-book-pdfs-{RUN_ENV if RUN_ENV != 'local' else 'dev'}"
 def healthz():
     return Response(status_code=200, content_type=content_types.APPLICATION_JSON, body={"message": "UP"})
 
-# @app.get("/api/v1/student/profile")
-# def student_get_profile():
-#     try:
-#         username = app.current_event.request_context.authorizer['username']
-#     except:
-#         username = 'dsingh14@illinois.edu'
-#     try:
-#         db_connection = get_db_connection(db_config, "resume_book_get_profile")
-#         with db_connection.transaction():
-#             with db_connection.cursor() as cur:
-#                 cur.execute(GET_USER_PROFILE_QUERY, [username])
-#                 profile_data = cur.fetchone()
-#         if not profile_data:
-#             DEFAULT_USER_PROFILE['username'] = username
-#             DEFAULT_USER_PROFILE['email'] = username
-#             return Response(status_code=200, content_type=content_types.APPLICATION_JSON, body=DEFAULT_USER_PROFILE)
-#     except Exception as e:
-#         logger.error(traceback.format_exc())
-#         return Response(status_code=500, content_type=content_types.APPLICATION_JSON, body={"message": "Error getting profile data", "details": str(e)})
-#     profile_data['resumePdfUrl'] = create_presigned_url_from_s3_url(profile_data['resumePdfUrl'])
-#     return Response(status_code=200, content_type=content_types.APPLICATION_JSON, body=profile_data)
+@app.get("/api/v1/student/profile")
+def student_get_profile():
+    try:
+        username = app.current_event.request_context.authorizer['username']
+    except:
+        username = 'dsingh14@illinois.edu'
+    try:
+        db_connection = get_db_connection(db_config, "resume_book_get_profile")
+        with db_connection.transaction():
+            with db_connection.cursor() as cur:
+                cur.execute(GET_USER_PROFILE_QUERY, [username])
+                profile_data = cur.fetchone()
+        if not profile_data:
+            DEFAULT_USER_PROFILE['username'] = username
+            DEFAULT_USER_PROFILE['email'] = username
+            return Response(status_code=200, content_type=content_types.APPLICATION_JSON, body=DEFAULT_USER_PROFILE)
+    except Exception as e:
+        logger.error(traceback.format_exc())
+        return Response(status_code=500, content_type=content_types.APPLICATION_JSON, body={"message": "Error getting profile data", "details": str(e)})
+    profile_data['resumePdfUrl'] = create_presigned_url_from_s3_url(profile_data['resumePdfUrl'])
+    return Response(status_code=200, content_type=content_types.APPLICATION_JSON, body=profile_data)
 
-# @app.post("/api/v1/student/profile")
-# def student_post_profile():
-#     email = app.current_event.request_context.authorizer['username']
-#     json_body: dict = app.current_event.json_body or {}
-#     json_body['email'] = email
-#     json_body['username'] = email
-#     json_body['resumePdfUrl'] = f"s3://{S3_BUCKET}/resume_{email}.pdf"
-#     try:
-#         data = json.loads(StudentProfileDetails(**json_body).model_dump_json(), parse_float=Decimal)
-#         dynamo_data = convert_to_dynamodb_json(preprocess_profile(data))
-#         dynamodb.put_item(TableName=PROFILE_TABLE_NAME, Item=dynamo_data)
-#     except pydantic.ValidationError as e:
-#         return Response(status_code=403, content_type=content_types.APPLICATION_JSON, body={"message": "Error validating payload", "details": str(e)})
-#     except Exception as e:
-#         logger.error(traceback.format_exc())
-#         return Response(status_code=500, content_type=content_types.APPLICATION_JSON, body={"message": "Error storing profile data", "details": str(e)})
-#     return Response(status_code=201, content_type=content_types.APPLICATION_JSON, body={"message": "Profile saved"})
+@app.post("/api/v1/student/profile")
+def student_post_profile():
+    email = app.current_event.request_context.authorizer['username']
+    json_body: dict = app.current_event.json_body or {}
+    json_body['email'] = email
+    json_body['username'] = email
+    json_body['resumePdfUrl'] = f"s3://{S3_BUCKET}/resume_{email}.pdf"
+    try:
+        data = json.loads(StudentProfileDetails(**json_body).model_dump_json(), parse_float=Decimal)
+        dynamo_data = convert_to_dynamodb_json(preprocess_profile(data))
+        dynamodb.put_item(TableName=PROFILE_TABLE_NAME, Item=dynamo_data)
+    except pydantic.ValidationError as e:
+        return Response(status_code=403, content_type=content_types.APPLICATION_JSON, body={"message": "Error validating payload", "details": str(e)})
+    except Exception as e:
+        logger.error(traceback.format_exc())
+        return Response(status_code=500, content_type=content_types.APPLICATION_JSON, body={"message": "Error storing profile data", "details": str(e)})
+    return Response(status_code=201, content_type=content_types.APPLICATION_JSON, body={"message": "Profile saved"})
 
-# @app.post("/api/v1/student/resume_upload_url")
-# def student_get_s3_presigned():
-#     try:
-#         username = app.current_event.request_context.authorizer['username']
-#         json_body: dict = app.current_event.json_body or {}
-#         data = json.loads(ResumeUploadPresignedRequest(**json_body).model_dump_json(), parse_float=Decimal)
-#     except pydantic.ValidationError as e:
-#         return Response(status_code=403, content_type=content_types.APPLICATION_JSON, body={"message": "Error validating payload", "details": str(e)})
-#     except Exception as e:
-#         logger.error(traceback.format_exc(), flush=True)
-#         return Response(status_code=500, content_type=content_types.APPLICATION_JSON, body={"message": "Error creating S3 URL", "details": str(e)})
-#     if data['file_size'] > 1.5e7: # 15 MB
-#         return Response(status_code=413, content_type=content_types.APPLICATION_JSON, body={"message": "Error creating S3 URL", "details": "Resume PDF cannot be larger than 15 MB."})
-#     presigned_url = create_presigned_url_for_put(bucket_name=S3_BUCKET, object_key=f"resume_{username}.pdf", file_size=data['file_size'])
-#     return Response(status_code=200, content_type=content_types.APPLICATION_JSON, body={"message": "URL created", "url": presigned_url})
+@app.post("/api/v1/student/resume_upload_url")
+def student_get_s3_presigned():
+    try:
+        username = app.current_event.request_context.authorizer['username']
+        json_body: dict = app.current_event.json_body or {}
+        data = json.loads(ResumeUploadPresignedRequest(**json_body).model_dump_json(), parse_float=Decimal)
+    except pydantic.ValidationError as e:
+        return Response(status_code=403, content_type=content_types.APPLICATION_JSON, body={"message": "Error validating payload", "details": str(e)})
+    except Exception as e:
+        logger.error(traceback.format_exc(), flush=True)
+        return Response(status_code=500, content_type=content_types.APPLICATION_JSON, body={"message": "Error creating S3 URL", "details": str(e)})
+    if data['file_size'] > 1.5e7: # 15 MB
+        return Response(status_code=413, content_type=content_types.APPLICATION_JSON, body={"message": "Error creating S3 URL", "details": "Resume PDF cannot be larger than 15 MB."})
+    presigned_url = create_presigned_url_for_put(bucket_name=S3_BUCKET, object_key=f"resume_{username}.pdf", file_size=data['file_size'])
+    return Response(status_code=200, content_type=content_types.APPLICATION_JSON, body={"message": "URL created", "url": presigned_url})
 
-# @app.get("/api/v1/recruiter/view_profile/<username>")
-# def recruiter_get_profile(username):
-#     try:
-#         resp = dynamodb.get_item(
-#             TableName=PROFILE_TABLE_NAME,
-#             Key={
-#                 'username': {'S': username}
-#             }
-#         )
-#         if 'Item' in resp:
-#             profile_data = resp['Item']
-#         else:
-#             return Response(status_code=404, content_type=content_types.APPLICATION_JSON, body={"message": f"No profile found for {username}"})
-#     except Exception as e:
-#         logger.error(traceback.format_exc())
-#         return Response(status_code=500, content_type=content_types.APPLICATION_JSON, body={"message": "Error getting profile data", "details": str(e)})
-#     # generate presigned url for resume pdf 
-#     profile_data = convert_from_dynamodb_json(profile_data)
-#     profile_data['resumePdfUrl'] = create_presigned_url_from_s3_url(profile_data['resumePdfUrl'])
-#     return Response(status_code=200, content_type=content_types.APPLICATION_JSON, body=profile_data)
+@app.get("/api/v1/recruiter/view_profile/<username>")
+def recruiter_get_profile(username):
+    try:
+        resp = dynamodb.get_item(
+            TableName=PROFILE_TABLE_NAME,
+            Key={
+                'username': {'S': username}
+            }
+        )
+        if 'Item' in resp:
+            profile_data = resp['Item']
+        else:
+            return Response(status_code=404, content_type=content_types.APPLICATION_JSON, body={"message": f"No profile found for {username}"})
+    except Exception as e:
+        logger.error(traceback.format_exc())
+        return Response(status_code=500, content_type=content_types.APPLICATION_JSON, body={"message": "Error getting profile data", "details": str(e)})
+    # generate presigned url for resume pdf 
+    profile_data = convert_from_dynamodb_json(profile_data)
+    profile_data['resumePdfUrl'] = create_presigned_url_from_s3_url(profile_data['resumePdfUrl'])
+    return Response(status_code=200, content_type=content_types.APPLICATION_JSON, body=profile_data)
 
-# @app.post("/api/v1/recruiter/search")
-# def recruiter_perform_search():
-#     json_body: dict = app.current_event.json_body or {}
-#     final_response = []
-#     try:
-#         data = json.loads(ProfileSearchRequest(**json_body).model_dump_json(), parse_float=Decimal)
-#         filter_query = construct_filter_query(data['degreeOptions'], data['gpa'], data['graduationYears'], data['majors'])
-#         dynamodb_resource = session.resource('dynamodb')
-#         table = dynamodb_resource.Table(PROFILE_TABLE_NAME)
-#         response = table.scan(
-#             FilterExpression=filter_query
-#         )
-#         if 'Items' in response:
-#             final_response = [unpreprocess_profile(x) for x in response['Items']]
-#     except pydantic.ValidationError as e:
-#         return Response(status_code=403, content_type=content_types.APPLICATION_JSON, body={"message": "Error validating payload", "details": str(e)})
-#     except Exception as e:
-#         logger.error(traceback.format_exc())
-#         return Response(status_code=500, content_type=content_types.APPLICATION_JSON, body={"message": "Error performing profile search", "details": str(e)})
-#     return Response(status_code=200, content_type=content_types.APPLICATION_JSON, body=final_response)
+@app.post("/api/v1/recruiter/search")
+def recruiter_perform_search():
+    json_body: dict = app.current_event.json_body or {}
+    final_response = []
+    try:
+        data = json.loads(ProfileSearchRequest(**json_body).model_dump_json(), parse_float=Decimal)
+        filter_query = construct_filter_query(data['degreeOptions'], data['gpa'], data['graduationYears'], data['majors'])
+        dynamodb_resource = session.resource('dynamodb')
+        table = dynamodb_resource.Table(PROFILE_TABLE_NAME)
+        response = table.scan(
+            FilterExpression=filter_query
+        )
+        if 'Items' in response:
+            final_response = [unpreprocess_profile(x) for x in response['Items']]
+    except pydantic.ValidationError as e:
+        return Response(status_code=403, content_type=content_types.APPLICATION_JSON, body={"message": "Error validating payload", "details": str(e)})
+    except Exception as e:
+        logger.error(traceback.format_exc())
+        return Response(status_code=500, content_type=content_types.APPLICATION_JSON, body={"message": "Error performing profile search", "details": str(e)})
+    return Response(status_code=200, content_type=content_types.APPLICATION_JSON, body=final_response)
