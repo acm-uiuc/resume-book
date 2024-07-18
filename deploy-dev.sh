5eb12ee0192586f027d2f0efb47a53a42870c05c
Dev Singh <dsingh14@illinois.edu>
2024-07-17 23:38:02 -0400

    cleanup

--- deploy-dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sam build --template-file cloudformation/lambda.yml
-sam deploy --no-confirm-changeset --no-fail-on-empty-changeset --parameter-overrides ParameterKey=ApiDomainName,ParameterValue=resume-api-dev.acm.illinois.edu ParameterKey=ApiCertificateArn,ParameterValue=arn:aws:acm:us-east-1:427040638965:certificate/07a0f957-15ae-4330-be14-25814728b981 ParameterKey=RunEnvironment,ParameterValue=dev
