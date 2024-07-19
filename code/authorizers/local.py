c2071570583acf8ef7ec4887aff0e4c6909bf291
Dev Singh <dsingh14@illinois.edu>
2024-07-19 19:53:14 -0400

    validate kinde tokens

--- code/authorizers/local.py
+++ code/authorizers/local.py
@@ -1,6 +1,24 @@
 from roles import setRolePolicies
 from shared import AuthPolicy
 import jwt
+import requests
+import json
+
+def get_jwks():
+    jwks_url = "https://auth.acm.illinois.edu/.well-known/jwks.json"
+    response = requests.get(jwks_url)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_public_key(jwks, kid):
+    for jwk in jwks["keys"]:
+        if jwk["kid"] == kid:
+            return jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(jwk))
+    raise Exception("Public key not found.")
+
+
+ALGORITHMS = ["RS256"]
 
 def lambda_handler(event, context):
     method, token = event['authorizationToken'].split(' ')
@@ -15,7 +33,10 @@ def lambda_handler(event, context):
     policy.region = tmp[3]
     policy.stage = apiGatewayArnTmp[1]
     if method == "Bearer" and token != "" and token:
-        decoded = jwt.decode(token, options={"verify_aud": False, "verify_signature": False},)
+        unverified_header = jwt.get_unverified_header(token)
+        jwks = get_jwks()
+        rsa_key = get_public_key(jwks, unverified_header["kid"])
+        decoded = jwt.decode(token, rsa_key, algorithms=ALGORITHMS, options={"verify_aud": False},)
         if "recruiter:resume-book" in decoded['permissions']:
             setRolePolicies('recruiter', policy)
         else:
