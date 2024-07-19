c55da18e1ec0c5e0bff8642fb93621ba1823de85
Dev Singh <dsingh14@illinois.edu>
2024-07-19 16:03:05 -0400

    update prompt 2

--- code/util/oai_prompts.py
+++ code/util/oai_prompts.py
@@ -20,7 +20,6 @@ export interface DegreeListing
   gpa: number;
 
 export interface StudentProfileDetails
-  username: string;
   name: string;
   email: string;
   linkedin?: string;
@@ -31,16 +30,16 @@ export interface StudentProfileDetails
   skills: string[];
   work_auth_required: boolean;
   sponsorship_required: boolean;
-  resumePdfUrl: string;
 
-All URLs start with https://, if field not available output an empty string. No username or resumePdfUrl.
+All URLs start with https://, if field not available output an empty string.
 BS/MCS is a degree at that is a combined Bachelor's and Master's (Non-Thesis) program. Output 2 degrees, MCS starting one year before program end.
-Same for BS/MS except it is Master's (Thesis).
+Same for BS/MS except it is Master's (Thesis) and takes 2 years.
 
 Graduate degrees do not have minors.
 
 The bio should be in the first person and should describe the candidate meaningfully, not just entirely reiterate the resume.
-It should be assumed the reader knows that they are a student at the University of Illinois Urbana-Champaign. Limit the bio to four sentences, be succinct. 
+It should be assumed the reader knows that they are a student at the University of Illinois Urbana-Champaign. Limit the bio to three sentences, be succinct. 
+You should not write generic bios; instead, the bio should call upon broad themes in the user's resume to convince recruiters how they would be a good fit for the job function they are looking for.
 The bio should tell the reader what the user's previous experience is in, and what type of roles they are looking for, and how they can contribute to a business in their role.
 The bio should be humble while also making clear what makes them a unique candidate for the position. 
 For the skills section, highlight skills that make the resume more attractive as a software engineer for the specific type of role they are seeking. 
