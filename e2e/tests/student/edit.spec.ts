b9ed073e605ca61351c6155e9f96fcc781e36b1f
Dev Singh <dsingh14@illinois.edu>
2024-09-11 15:46:58 -0500

    fix/add tests

--- e2e/tests/student/edit.spec.ts
+++ e2e/tests/student/edit.spec.ts
@@ -10,12 +10,22 @@ describe("Test that users can edit their profile", () => {
     expect(page.getByText('Skills')).toBeTruthy()
     expect(page.getByText('Botting')).toBeTruthy()
     await page.getByRole('button', { name: 'Edit' }).click();
-    if (!page.getByRole('button', { name: 'Add Degree' }).isVisible()) {
+    if (!(await page.getByRole('button', { name: 'Remove' }).nth(1).isVisible())) {
       await page.getByRole('button', { name: 'Add Degree' }).click();
       await page.getByRole('textbox', { name: 'Major' }).click();
       await page.getByRole('option', { name: 'Computer Science', exact: true }).click();
     }
     await page.getByRole('button', { name: 'Save' }).click();
     expect(await page.waitForSelector('text="Profile saved!"')).toBeTruthy();
+  });
+  test('Profiles with no degrees fail to save', async ({ page, becomeUser }) => {
+    await becomeUser(page, {email: 'nonexistent@testing.illinois.edu', role: 'student'})
+    await page.getByRole('link', { name: 'My Profile' }).click();
+    expect(page.getByText('Resume Book User')).toBeTruthy()
+    expect(page.getByText('Skills')).toBeTruthy()
+    expect(page.getByText('Botting')).toBeTruthy()
+    await page.getByRole('button', { name: 'Edit' }).click();
+    await page.getByRole('button', { name: 'Save' }).click();
+    expect(await page.waitForSelector('text="You must specify at least one degree."')).toBeTruthy();
   });
 })
