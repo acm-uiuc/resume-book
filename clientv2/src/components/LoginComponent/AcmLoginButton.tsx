4f1df2d46ce25374f13562d9e5a92d0831a768ac
Dev Singh <dsingh14@illinois.edu>
2024-09-15 14:47:00 -0500

    fix lint

--- clientv2/src/components/LoginComponent/AcmLoginButton.tsx
+++ clientv2/src/components/LoginComponent/AcmLoginButton.tsx
@@ -1,9 +1,8 @@
-import { Button, ButtonProps } from '@mantine/core';
+import { Button, ButtonProps, Image } from '@mantine/core';
 import { useMsal } from '@azure/msal-react';
 import { InteractionStatus } from '@azure/msal-browser';
 import { useAuth } from '../AuthContext';
-import { Image } from '@mantine/core';
-import blockI from '@/blockI.png'
+import blockI from '@/blockI.png';
 
 export function AcmLoginButton(props: ButtonProps & React.ComponentPropsWithoutRef<'button'>) {
   const { loginMsal } = useAuth();
@@ -11,7 +10,12 @@ export function AcmLoginButton(props: ButtonProps & React.ComponentPropsWithoutR
   return (
     <Button
       disabled={inProgress === InteractionStatus.Login}
-      leftSection={<Image src={blockI} style={{'height': '1.5em', 'width': 'auto', 'paddingRight': '0.8em'}}></Image>}
+      leftSection={
+        <Image
+          src={blockI}
+          style={{ height: '1.5em', width: 'auto', paddingRight: '0.8em' }}
+        ></Image>
+      }
       color="#FF5F05"
       variant="filled"
       {...props}
